7481a4bab82bcf0e61f7698739de383f0c5814d6
vicmartinez <vmmartinez.infomec19@gmail.com>
2026-09-09 01:42:54 -0600

    feat: implementar division de texto en palabras usando split

--- slidingWindow.js
+++ slidingWindow.js
@@ -1,6 +1,13 @@
+
 export function findLongestWord(text) {
-  let longestWord = '';
+  if (!text || typeof text !== 'string') return '';
+
+  const words = text.split(' ');
+  let longestWord = words[0] || '';
+
   return longestWord;
 }
 
-console.log('Palabra inicial:', findLongestWord("Hola mundo"));
+const texto = "JavaScript es un lenguaje asombroso";
+console.log('Texto:', texto);
+console.log('Primera palabra detectada:', findLongestWord(texto));
